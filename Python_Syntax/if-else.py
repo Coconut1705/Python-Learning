2a46e29a245e32cf71573402fc2a25b7464fbff8
Nguyen Phu Dat <datdua2015@gmail.com>
2026-01-16 15:59:07 +0700

    cap nhat nua ne

--- Python_Syntax/if-else.py
+++ Python_Syntax/if-else.py
@@ -4,3 +4,13 @@ if passcode == 1705:
     print("Ô Kê dô đi cưng")
 else:
     print("Mật khẩu sai cmnr, nhập lại đi")
+
+
+score = 8
+
+if score >= 8:
+    print("Bạn giỏi vãi")
+    if score == 10:
+        print("Bạn đỉnh vãi ò")
+    else:
+        print("Nhưng mà cần cố hơn")
