8b0fc715c59007e26e5d51993f3e58983511fd49
Nguyen Phu Dat <datdua2015@gmail.com>
2026-01-22 17:20:40 +0700

    tiep nua ne

---
+++ PYTHON_BASIC/Basic_testing.py
@@ -0,0 +1,39 @@
+a = "MOT HAI BA BON NAM SAU"
+print(a.lower())
+
+b = "có một con cá"
+print(b.upper())
+
+c = " Tui tên là Coconut"
+print(c.strip())
+
+d = "Tui tên là Coconut " 
+print(d)
+
+e = "Có cái ò"
+print(e.replace("ò", "gì ?"))
+
+f = "HOHOHOHH,HÍ HÍ HÍ HÍ"
+print(f.split(","))
+
+g = "Emmmm"
+h = "Meooooo"
+i = g + " " + h
+print(i)
+
+age = 21
+name = f"Nếu tôi tên Dừa, tôi {age} tuổi"
+print(name)
+
+giá = 5000000 + 3000000
+price = f"Giá của cái màn hình iPhone này là {giá:.2f} VNĐ" 
+print(price)
+
+txt = "Tôi tên là \"Coconut\" aka Dừa"
+print(txt)
+
+abc = "Hello"
+print(abc.startswith("H"))
+
+txt = "Hello World"
+print(txt[0])
